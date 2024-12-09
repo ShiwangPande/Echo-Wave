9ccec97d70ed110a3f5f6d974f718a8972b04808
ShiwangPande <srpandey@gcoen.ac.in>
2024-12-09 22:45:19 +0530

    final

--- middleware.ts
+++ middleware.ts
@@ -15,7 +15,7 @@ export default clerkMiddleware(async (auth, request) => {
 }, {
   publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   secretKey: process.env.CLERK_SECRET_KEY,
-  domain: 'https://echo-wave-pied.vercel.app',  // Example: 'clerk.dev'
+  domain: 'https://echo-wave-pied.vercel.app',  
   signInUrl: '/sign-in',  // Redirect to this URL if unauthenticated
   signUpUrl: '/sign-up',  // Redirect to this URL if unauthenticated
   afterSignInUrl: '/',  // Redirect here after successful sign-in
