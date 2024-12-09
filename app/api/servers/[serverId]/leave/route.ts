6836763664aeea267a9620ddb7eda59fe4866fab
ShiwangPande <srpandey@gcoen.ac.in>
2024-12-09 20:24:43 +0530

    final

--- app/api/servers/[serverId]/leave/route.ts
+++ app/api/servers/[serverId]/leave/route.ts
@@ -2,44 +2,43 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function PATCH(
-    req: Request,
-    {params}:{ params: {serverId: string} }
-) {
-    try{
-        const { serverId } = await params;
-        const profile = await currentProfile();
+export async function PATCH(req: Request, { params }: { params: Promise<{ serverId: string }> }) {
+  try {
+    const { serverId } = await params;
+    const profile = await currentProfile();
 
-        if(!profile){
-            return new NextResponse("Unauthorized", {status:401});
-        }
-        if(!serverId){
-            return new NextResponse("Server ID missing", {status:400});
-        }
-
-        const server = await db.server.update({
-            where:{
-                id: serverId,
-                profileId:{
-                    not: profile.id
-                },
-                members:{
-                    some:{
-                        profileId: profile.id
-                    }
-                }
-            },
-            data:{
-                members:{
-                    deleteMany:{
-                        profileId:profile.id
-                    }
-                }
-            }
-        })
-        return NextResponse.json(server);
-    }catch(error){
-        console.log("[SERVER_ID_LEAVE]",error);
-        return new NextResponse("Internal Error", {status:500})
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+    if (!serverId) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+    const requestBody = await req.json(); // If the body contains JSON
+    console.log("Request Body:", requestBody);
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: {
+          not: profile.id,
+        },
+        members: {
+          some: {
+            profileId: profile.id,
+          },
+        },
+      },
+      data: {
+        members: {
+          deleteMany: {
+            profileId: profile.id,
+          },
+        },
+      },
+    });
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_LEAVE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
